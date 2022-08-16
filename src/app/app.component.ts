b28f172aafc7bdbbc3c8ca6667de27531989c883
camiloF1 <cami.floresl@duocuc.cl>
2022-08-16 17:04:02 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'mi_primera_vez_con_angular';
+}
