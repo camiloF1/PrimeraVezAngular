d2849253e26c30e3fe2d24ee3566cc84d3e18d5f
camiloF1 <cami.floresl@duocuc.cl>
2022-08-16 18:11:30 -0400

    primercommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mi_primera_vez_con_angular';
+  edad: number = 21;
+  direccion: string = "calle falsa 212"
+  rol: String= "rol falso"
+  sumar(numero1:number,numero2:number): number {
+    return numero1 + numero2;
+  }
+  restar(numero1:number,numero2:number): number {
+    return numero1 - numero2;
+  }
+  multiplicar(numero1:number,numero2:number): number{
+    return numero1 * numero2;
+  }
 }
